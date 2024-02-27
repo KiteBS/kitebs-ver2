2c2b5a0245a79c8ca4cbf5c1013f12cfeb8580d7
Tri Thien Nguyen <anretrithien@gmail.com>
2024-02-27 14:57:35 +0000

    update

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,8 +1,11 @@
 import DetailsFooter from "@/components/DetailsFooter";
 import NavBar from "@/components/NavBar";
-import { SignIn } from "@clerk/nextjs";
+import { auth, currentUser } from "@clerk/nextjs";
 
-export default function Page() {
+export default async function Page() {
+  const { userId } = await auth();
+  const user = await currentUser();
+  const isAuth = !!userId;
   return (
     <>
       <NavBar />
@@ -10,6 +13,18 @@ export default function Page() {
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
           <div className="flex flex-col items-center text-center">
             <div className="flex items-center mt-10">
+              <div>
+                {" "}
+                {isAuth && (
+                  <div>
+                    <h1 className="text-4xl font-semibold">
+                      Hi {user?.firstName}
+                    </h1>
+                  </div>
+                )}
+              </div>
+            </div>
+            <div>
               <h1>Dashboard</h1>
             </div>
           </div>
